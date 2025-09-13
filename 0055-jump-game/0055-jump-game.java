1e8394660864c6f0a4c70450825efafbfe1b575b
Dheeru01 <141866112+Dheeru01@users.noreply.github.com>
2025-09-13 20:41:45 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,20 +1,15 @@
 class Solution {
     public boolean canJump(int[] nums) {
         int n = nums.length;
-        boolean[] list = new boolean[n];
-        list[n-1] = true;
-        for(int i=n-2; i>=0 ; i--){
-            if(nums[i] != 0){
-                for(int j=1;j<=nums[i];j++){
-                    if(list[i+j] == true){
-                        list[i] = true;
-                        break;
-                    }
-                }
-            }
+        int max_ind = 0;
+        for(int i=0; i<n; i++){
             
+            if(i > max_ind) return false;
+            if((i + nums[i]) > max_ind ){
+                max_ind += nums[i];
+            }
         }
 
-        return list[0];
+        return true;
     }
 }
