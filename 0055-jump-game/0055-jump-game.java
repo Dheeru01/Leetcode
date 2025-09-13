7bc81695cf194a804b4ef18d4fea08da760de226
Dheeru01 <141866112+Dheeru01@users.noreply.github.com>
2025-09-13 20:41:54 +0530

    Time: 2 ms (83.84%), Space: 45.9 MB (43.72%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -5,9 +5,7 @@ class Solution {
         for(int i=0; i<n; i++){
             
             if(i > max_ind) return false;
-            if((i + nums[i]) > max_ind ){
-                max_ind += nums[i];
-            }
+            max_ind = Math.max(max_ind, i + nums[i]);
         }
 
         return true;
