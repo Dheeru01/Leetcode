5804e9a47c1c245dfeb59f3fcfe27d853b9a1bc1
Dheeru01 <141866112+Dheeru01@users.noreply.github.com>
2025-09-15 21:51:14 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int n = coins.length;
+        Integer[][] dp = new Integer[n][amount+1];
+        return (f(n-1,coins,amount,dp) == Integer.MAX_VALUE)? -1 :f(n-1,coins,amount,dp) ;
+        
+    }
+    int f(int i ,int[] coins, int amount,Integer[][] dp ){
+        if(amount == 0){
+            return 0;
+        }
+        if(i==0){
+            
+            if(amount % coins[0] == 0) return amount/coins[0];
+            
+            return Integer.MAX_VALUE;
+        }
+        
+        
+        if(dp[i][amount] != null){
+            return dp[i][amount];
+        }
+
+        
+        int not_pick = f(i-1,coins,amount,dp);
+        int pick =Integer.MAX_VALUE; 
+        if(amount >= coins[i]){
+            pick = 1 + f(i,coins,amount-coins[i],dp);
+        }
+
+        return dp[i][amount] = Math.min(pick,not_pick);
+    }
+}
